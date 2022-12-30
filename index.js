eba255212684f716ec7db64910e1ca99f2324403
Denis Seleznev <hcodes@users.noreply.github.com>
2022-12-30 20:24:52 +0300

    Update deps in package.json

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const fast_average_color_node_1 = require("fast-average-color-node");
-fast_average_color_node_1.getAverageColor('./sunflower.jpg').then((result) => {
+(0, fast_average_color_node_1.getAverageColor)('./sunflower.jpg').then((result) => {
     console.log('result', result);
 });
